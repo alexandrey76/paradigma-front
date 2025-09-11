eb0364e921c2f59e9bdd65a7ffa63b9ae5b401fd
alexandrey76 <aleksandralekseev7620@gmail.com>
2025-09-12 02:34:51 +0300

    test

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ const products = [
     name: "Электронный кальян Paradigma One",
     description: "Описание товара 1",
     price: 17_990,
-    images: ["/images/product1_1.jpg","/images/product1_2.jpg", "/images/product1_3.jpg"],
+    images: ["/products_images/product1_1.jpg","/products_images/product1_2.jpg", "/products_images/product1_3.jpg"],
     videos: ["/videos/product1-1_new.mp4"]
 
 },
