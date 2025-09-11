5eae8572ad6f4df5c5710242a97a75527fd32967
alexandrey76 <aleksandralekseev7620@gmail.com>
2025-09-12 02:32:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-// src/App.js
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // или BrowserRouter
-import { CartProvider } from "./context/CartContext";  // как у тебя сделано
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import CatalogPage from "./pages/CatalogPage";
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
-import NavBar from "./components/NavBar";
-import FloatingBubble from "./components/FloatingBubble";
 
 export default function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   );
 }
