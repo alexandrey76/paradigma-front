47782f95d33ff4bc086c33bb7d0c612a45c017bf
alexandrey76 <aleksandralekseev7620@gmail.com>
2025-09-12 18:42:04 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
+        {/* отрисовать по пустому хэшу */}
+        <Route index element={<HomePage />} />
+        {/* обычный / тоже на всякий */}
         <Route path="/" element={<HomePage />} />
+        {/* любой другой путь — редирект на / */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
   );
