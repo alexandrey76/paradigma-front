40bd65dfdf51ed82cd6e247aab658210f8ba03a2
alexandrey76 <aleksandralekseev7620@gmail.com>
2025-09-13 17:16:48 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import GlobalStyle from "./styles/GlobalStyle";
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
 import NavBar from "./components/NavBar";
+import React, { useEffect } from "react";
 
 export default function App() {
+   useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      tg.ready();   // сообщает Telegram, что всё готово
+      tg.expand();  // сразу развернуть на максимум
+    }
+  }, []);
   return (
     <HashRouter>
       <GlobalStyle />   {/* ← глобальный фон и сбросы работают на всех страницах */}
