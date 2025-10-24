e45873f51528ce078f504a82f97b794d86e437dd
Aleksandr Alekseev <aleksandralekseev7620@gmail.com>
2025-10-24 13:56:38 +0300

    test

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -176,7 +176,7 @@ export default function OrdersPage() {
 
                 <div>
                   <FieldLabel>Статус</FieldLabel>
-                  <StatusPillSvg status={o.status} size={24}/>
+                  <StatusPillSvg status={o.status} size={10}/>
                 </div>
 
                 <SeeBtn type="button" onClick={(e) => handleSeeButtonClick(e, o.id)}>
